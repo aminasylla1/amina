85b2463c6206ef5edc14277cc9b5a738c109689d
Amina <aminasylla145@gmail.com>
2026-04-24 12:48:09 +0000

    modification du projet

--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 int main()
 {
+    int nb, inverse = 0;
     cout <<"Bonjour c'est Tantie Ami"<< endl; // Amina
+      cout << "entrez un nombre:";
+      cin >> nb;
+      while (nb !=0)
+      {
+        int chiffre = nb % 10;
+        inverse = inverse * 10 + chiffre;
+        nb = nb / 10;
+      }
+      cout <<"le nombre inverse est:"<<inverse<<endl;
     return 0;
 }
