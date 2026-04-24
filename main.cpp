cbb9ada30ec5e7d41118f99aa32da0d6b3b97f36
Amina <aminasylla145@gmail.com>
2026-04-24 13:20:57 +0000

    modification du projet

--- main.cpp
+++ main.cpp
@@ -14,8 +14,5 @@ int main()
         nb = nb / 10;
       }
       cout <<"le nombre inverse est:"<<inverse<<endl;
-=======
-    //ecrire un programme c++ qui permet d'inverser un nombre saisi par l'utilisateur 
->>>>>>> 7757505616a56b647081c64a726e84c0109f1463
     return 0;
 }
